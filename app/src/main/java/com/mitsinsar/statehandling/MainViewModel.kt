db701aa8b2e5076991ae6d28116a28853902ab48
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2022-01-22 07:21:44 +0300

    ♻️ Prevent state loss on process death

--- app/src/main/java/com/mitsinsar/statehandling/MainViewModel.kt
+++ app/src/main/java/com/mitsinsar/statehandling/MainViewModel.kt
@@ -1,22 +1,33 @@
 package com.mitsinsar.statehandling
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     val userStateFlow: StateFlow<UserUiState>
         get() = _userStateFlow
-    private val _userStateFlow = MutableStateFlow(UserUiState.create())
+    private val _userStateFlow = MutableStateFlow(
+        savedStateHandle.get<UserUiState>(USER_UI_STATE_KEY) ?: UserUiState.create()
+    )
 
     fun getUser() {
-        _userStateFlow.value = updateUserUiState()
+        val userUiState = updateUserUiState()
+        _userStateFlow.value = userUiState
+        savedStateHandle.set(USER_UI_STATE_KEY, userUiState)
     }
 
     private fun updateUserUiState(): UserUiState {
         return userStateFlow.value.copy(
             user = DummyUserProvider.createDummyUser()
         )
+    }
+
+    companion object {
+        const val USER_UI_STATE_KEY = "user_ui_state"
     }
 }
